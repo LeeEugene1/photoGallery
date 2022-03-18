b1231a7a2c2599a73cf72852ae0451d45cb1b96e
LeeEugene1 <uwm1004@gmail.com>
2022-03-18 23:47:06 +0900

    imageList

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import logo from "./logo.svg";
 import "./App.scss";
 import ImageBox from "./components/ImageBox";
 
 function App() {
+  const inpRef = useRef<HTMLInputElement>(null);
+  const [imageList, setImageList] = useState<string[]>([]);
+  console.log(imageList);
+  const inpRefClick = () => {
+    inpRef.current?.click();
+  };
+  const inpRefChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    if (e.currentTarget.value) {
+      let elem = e.currentTarget.value;
+      setImageList((prev) => [...prev, elem]);
+    }
+  };
   return (
     <section>
       <div className="imageUpload">
-        <div className="imageUpload__content">
-          There is no image.
-          <br />
-          Post your first photo.
+        {imageList.length === 0 && (
+          <div className="imageUpload__content">
+            There is no image.
+            <br />
+            Post your first photo.
+          </div>
+        )}
+        <input
+          className="imageUpload__input"
+          type="file"
+          ref={inpRef}
+          onChange={inpRefChange}
+        />
+        <div className="imageUpload__post--button" onClick={inpRefClick}>
+          +
         </div>
-        <div className="imageUpload__post--button">+</div>
         <ImageBox src="" />
       </div>
     </section>
